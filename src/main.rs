52fe0959fc5f87db90407cc2f6340907956db714
AnActualEmerald <kevinliger13@gmail.com>
2020-04-09 02:34:13 -0400

    Added clap as a dependency and started writing main program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Ipsos Watchlist Manager")
+        .version("0.0.1")
+        .author("Emerald")
+        .about("Manage your watchlist with ease from the command line")
+        .arg(
+            Arg::with_name("list")
+                .short("l")
+                .long("list")
+                .help("Show all the current watchlists"),
+        )
+        .subcommand(
+            SubCommand::with_name("new")
+                .about("Create a new watchlist")
+                .arg(
+                    Arg::with_name("NAME")
+                        .help("Name of the new watchlist")
+                        .required(true),
+                ),
+        )
+        .get_matches();
+    if matches.is_present("list") {
+        println!("A whole bunch of watchlists");
+    }
+
+    if let Some(matches) = matches.subcommand_matches("new") {
+        println!("{:?}", matches.value_of("NAME").unwrap());
+    }
 }
